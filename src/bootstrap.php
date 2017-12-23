0301e56f91b1b476cdcfc89e37d3801d970a1077
maxbon <bonin.maxime@gmail.com>
2017-12-23 14:43:16 -0500

    formating

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -4,39 +4,40 @@ declare(strict_types = 1);
 
 namespace biblos;
 
-/**
- * for error handling
- */
-require __DIR__ . '/../vendor/autoload.php';
-error_reporting(E_ALL);
-$environment = 'development';
-$whoops = new \Whoops\Run;
-if ($environment !== 'production') {
-    $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
-} else {
-    $whoops->pushHandler(function($e){
-        echo 'Todo: Friendly error page and send an email to the developer';
-    });
-}
-$whoops->register();
-/**
- * end of error handling
- */
+    /**
+     * for error handling
+     */
+    require __DIR__ . '/../vendor/autoload.php';
 
-/**
- * patrickLouys HTTP oop interface package
- */
-$request = new \Http\HttpRequest($_GET, $_POST, $_COOKIE, $_FILES, $_SERVER);
-$response = new \Http\HttpResponse;
+    error_reporting(E_ALL);
+    $environment = 'development';
 
-$content = '<h1>Hello the world of Open source fanatics !!!</h1>';
-$response->setContent($content);
+    $whoops = new \Whoops\Run;
 
-/**
- * output browser
- */
-foreach ($response->getHeaders() as $header) {
-    header($header, false);
-}
+    if ($environment !== 'production') {
+        $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+    } else {
+        $whoops->pushHandler(function($e){
+            echo 'Todo: Friendly error page and send an email to the developer';
+        });
+    }
 
-echo $response->getContent();
+    $whoops->register();
+
+    /**
+     * patrickLouys HTTP oop interface package
+     */
+    $request = new \Http\HttpRequest($_GET, $_POST, $_COOKIE, $_FILES, $_SERVER);
+    $response = new \Http\HttpResponse;
+
+    $content = '<h1>Hello the world of Open source fanatics !!!</h1>';
+    $response->setContent($content);
+
+    /**
+     * output to browser
+     */
+    foreach ($response->getHeaders() as $header) {
+        header($header, false);
+    }
+
+    echo $response->getContent();
